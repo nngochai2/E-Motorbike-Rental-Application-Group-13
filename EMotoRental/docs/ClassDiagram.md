ae4c7c59d67102e605bc1e4c8713604584339251
nngochai2 <nngochaial@gmail.com>
2025-08-15 13:32:17 +0700

    Add ClassDiagram

---
+++ EMotoRental/docs/ClassDiagram.md
@@ -0,0 +1,2 @@
+# draw.io:
+https://drive.google.com/file/d/1PHRKzh8QkKCBA_iYTLnexLMh3qtEdcjn/view?usp=sharing
---
+++ EMotoRental/src/enums/RentalStatus.h
@@ -0,0 +1,11 @@
+//
+// Created by Nguyen Ngoc Hai on 8/15/2025.
+//
+
+#pragma once
+namespace EmotoRental {
+    enum class RentalStatus {
+        ACTIVE,
+
+    }
+}
