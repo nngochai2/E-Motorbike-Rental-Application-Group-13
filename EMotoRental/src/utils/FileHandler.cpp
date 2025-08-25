da7026173e8e1ef0afc59d77e1c7db41066318c4
nngochai2 <nngochaial@gmail.com>
2025-08-25 23:13:42 +0700

    Create FileHandler

---
+++ EMotoRental/src/utils/FileHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nguyen Ngoc Hai on 8/25/2025.
+//
+
+#include "FileHandler.h"
---
+++ EMotoRental/src/utils/FileHandler.h
@@ -0,0 +1,29 @@
+//
+// Created by Nguyen Ngoc Hai on 8/25/2025.
+//
+
+#pragma once
+#include <string>
+#include <vector>
+
+namespace EMotoRental
+{
+    class FileHandler
+    {
+    public:
+        // Basic file operations
+        static bool writeToFile(const std::string& content, const std::string& fileName);
+        static std::string readFromFile(const std::string& fileName);
+        static bool fileExists(const std::string& fileName);
+        static bool createFile(const std::string& fileName);
+        static bool createBackup(const std::string& fileName);
+
+        // CSV parsing utils
+        static std::vector<std::string> parseCSVFile(const std::string& line);
+        static std::string formatCSVLine(const std::vector<std::string>& data);
+
+        // Directory operations
+        static bool createDirectory(const std::string& fileName);
+        static bool ensureDataDirectory();
+    };
+}
